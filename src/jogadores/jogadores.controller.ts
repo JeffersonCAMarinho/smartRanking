a7acd38684b9861c04985d9e0f41a8faaf9ad6eb
Marinho <jeffersonmarinho97@gmail.com>
2023-01-12 12:11:19 -0300

    feat: http exception treatment and validation pipe

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -10,11 +10,11 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros';
+import { ValidacaoParametrosPipe } from 'src/common/pipes/validacao-parametros';
+import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 import { JogadoresService } from './jogadores.service';
-import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -31,7 +31,7 @@ export class JogadoresController {
   @UsePipes(ValidationPipe)
   async atualizarJogador(
     @Body() atualizarJogadorDto: AtualizarJogadorDto,
-    @Param('_id', JogadoresValidacaoParametrosPipe) _id: string,
+    @Param('_id', ValidacaoParametrosPipe) _id: string,
   ): Promise<void> {
     this.jogadoresService.atualizarJogador(_id, atualizarJogadorDto);
   }
@@ -43,13 +43,13 @@ export class JogadoresController {
 
   @Get('/:_id')
   async consultarJogadorPeloId(
-    @Param('_id', JogadoresValidacaoParametrosPipe) _id: string,
+    @Param('_id', ValidacaoParametrosPipe) _id: string,
   ): Promise<Jogador> {
     return this.jogadoresService.consultarJogadoresPeloId(_id);
   }
   @Delete('/:_id')
   async deletarJogador(
-    @Query('_id', JogadoresValidacaoParametrosPipe) _id: string,
+    @Query('_id', ValidacaoParametrosPipe) _id: string,
   ): Promise<void> {
     this.jogadoresService.deletarJogador(_id);
   }
